a9fdc32ff454809f0cf1736f3cf0c00c38ccddc9
vaibhavSkillminds <vaibhav@skillminds.com>
2025-03-26 15:31:46 +0530

    serverless

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const axios = require('axios');
 const path = require("path");
 require('dotenv').config();
+const serverless = require('serverless-http');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -32,3 +33,5 @@ app.get('/api/data', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
+
+module.exports.handler = serverless(app);
